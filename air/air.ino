001b4172364d8954be4cd96cb53b363df6940d66
Safon686 <Safon686@gmail.com>
2021-10-28 01:11:15 +0700

    Fix build

--- air/air.ino
+++ air/air.ino
@@ -13,56 +13,46 @@ void setup()
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(17, INPUT);
-   pinMode(12, INPUT);
-   EEPROM.get(0, val1);
-   
-
+  pinMode(12, INPUT);
+  EEPROM.get(0, val1);
 }
 
-bool key_presesd(int port) {
+bool key_pressed(int port) {
   int value = analogRead(port);
   return value > 200;
 }
 
- }
 void loop()
 {
    val = analogRead(16); // считываем значение с входа
-   if (key_pressed(17)) {
-   
-   
+   if (key_pressed(17)) { 
     EEPROM.put(0, val);  //условно 500
     Serial.println("Wtite to EEPROM");
     Serial.println(val); 
     Serial.println("Read from EEPROM");
     Serial.println(val1);
-       delay(1000);
-  }
+    delay(1000);
+   }
   
    EEPROM.get(0, val1);
-    a=val1-50; //450
-    b=val1+50; //550
+   a=val1-50; //450
+   b=val1+50; //550
    if (val <= a) {
     digitalWrite(5, HIGH);  
      }
      else {
       digitalWrite(5, LOW);
      }
-  if (val >= b) {
-    digitalWrite(6, HIGH);  
-  }
-  else {
+   if (val >= b) {
+     digitalWrite(6, HIGH);  
+   }
+   else {
     digitalWrite(6, LOW);
+   }
+   if ( b > val && val > a) {
+      digitalWrite(13, HIGH);  
+   }
+   else {
+      digitalWrite(13, LOW);
   }
-    if ( b > val && val > a) {
-    
-  digitalWrite(13, HIGH);  
-  }
-  else {
-  digitalWrite(13, LOW);
-
-  
-  }
-  
-  
 }
