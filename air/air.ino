73d289fb837fd24607c82603dccf53f439f7f457
Safon686 <Safon686@gmail.com>
2021-10-28 21:04:44 +0700

    problem with valve

--- air/air.ino
+++ air/air.ino
@@ -1,12 +1,13 @@
 #include <EEPROM.h>
 
 int val,val1,val2;
-
+uint32_t timer;
 #define NOT_INITIALIZED -1
 
 int ledPin = 13;                 
 int a,b,z,x=0; 
 bool setup_height;
+int interval = 200;
 
 void setup()
 {
@@ -19,9 +20,22 @@ void setup()
   pinMode(18,INPUT);  //start preset button
   EEPROM.get(0, val1); //read preset 
   setup_height = false; //stat preset button
+  timer = millis();     //timer
+  long previosmillis = 0;
+  }
+void work_valve(int port) {           //function of operation of valves with interruption of opening
+  if (millis() - timer > interval) {  //from experience I can say that work with interruption is more stable for the formation of the suspension
+  digitalWrite(port, HIGH);
+  timer =millis(); 
+  }
+  if (millis()+100-timer>interval) {
+  digitalWrite(port, LOW);
+  }
+  
+  
+  
 }
-
-bool key_pressed(int port) {    //function read button
+bool key_pressed(int port) {          //function read button
   int value = analogRead(port);
   return value > 200;
 }
@@ -30,10 +44,10 @@ void loop()
 {
    val = analogRead(16);               //height sensor value (preset)
    if (key_pressed(17)) {              //calibrate preset button
-    EEPROM.put(0, val);                //write value 
-    Serial.println("Wtite to EEPROM");
-    Serial.println(val); 
-    delay(1000);
+   EEPROM.put(0, val);                 //write value 
+   Serial.println("Wtite to EEPROM");
+   Serial.println(val); 
+   delay(1000);
    }
       
    EEPROM.get(0, val1);                //read preset sensor value
@@ -41,21 +55,17 @@ void loop()
     setup_height = true;
    }
 
-   if (setup_height && val != val1) {  //
-   a=val1-50;                          //set variables with suspension error tolerance
+   if (setup_height && val != val1) { //
+   a=val1-50;                         //set variables with suspension error tolerance
    b=val1+50; //550
-   if (val <= a) {
-    digitalWrite(5, HIGH);            //if the current value is less than the preset, turn on the up valve
-     }
-     else {
-    digitalWrite(5, LOW);
-    }
+   if (val <= a)  {                    //if the current value is less than the preset, turn on the up valve
+    work_valve(5);                    
+   } 
+    
    if (val >= b) {                    //if the current value is less than the preset, turn the valve down
-    digitalWrite(6, HIGH);  
+    work_valve(6);
    }
-   else {
-    digitalWrite(6, LOW);
-   }
+    
    if ( b > val && val > a) {        //Suspension status indication
       digitalWrite(13, HIGH);
       setup_height = false;          //off function preset after val == val1
