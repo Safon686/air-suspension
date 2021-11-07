b6cd51f3ffa7e0cb541b21a7455c112331529dc0
Safon686 <Safon686@gmail.com>
2021-11-07 21:34:22 +0700

    new logiс work valve
    valves work progressively, at large deviations the valve is constantly open for fast operation. When the height sensor approaches the desired value, the valves start to work alternately, open / close every 100ms for a more accurate setting

--- air/air.ino
+++ air/air.ino
@@ -6,6 +6,8 @@ uint32_t timer;
 int ledPin = 13;
 int a, b, z, x = 0;
 bool setup_height;
+int conditionlow = 0;
+int conditionup = 0;
 int interval = 200;
 
 void setup() {
@@ -21,8 +23,8 @@ void setup() {
   timer = millis();     //timer
   long previosmillis = 0;
 }
-void work_valve(int port) {           //function of operation of valves with interruption of opening
-  if (millis() - timer > interval) {  //from experience I can say that work with interruption is more stable for the formation of the suspension
+void pre_work_valve(int port) {           //function of operation of valves with interruption of opening
+  if (millis() - timer > interval) {
     digitalWrite(port, HIGH);
     timer = millis();
   }
@@ -30,31 +32,49 @@ void work_valve(int port) {           //function of operation of valves with int
     digitalWrite(port, LOW);
   }
 }
-bool key_pressed(int port) {          //function read button
+void work_valve(int port) {               //function of operation of valves with interruption of opening
+  digitalWrite(port, HIGH);               //valve function when approaching the result
+}                                         //from experience I can say that work with interruption is more stable for the formation of the suspension
+bool key_pressed(int port) {              //function read button
   int value = analogRead(port);
   return value > 200;
 }
 void loop() {
-  val = analogRead(16);               //height sensor value (preset)
-  if (key_pressed(17)) {              //calibrate preset button
+  val = analogRead(16);                 //height sensor value (preset)
+  if (key_pressed(17)) {                //calibrate preset button
     EEPROM.put(0, val);                 //write value
     Serial.println("Wtite to EEPROM");
     Serial.println(val);
     delay(1000);
   }
 
-  EEPROM.get(0, val1);                //read preset sensor value
-  if (key_pressed(18)) {              //preset button
+  EEPROM.get(0, val1);                  //read preset sensor value
+  if (key_pressed(18)) {                //preset button
     setup_height = true;
   }
-  if (setup_height && val != val1) { //
-    a = val1 - 50;                     //set variables with suspension error tolerance
-    b = val1 + 50;
-    if (val < a) {                    //if the current value is less than the preset, turn on the up valve
+  Serial.println(val);
+  if (setup_height && val != val1) {   
+    conditionlow = 1;
+    conditionup = 1;
+    a = val1 - 20;                     //set variables with suspension error tolerance
+    b = val1 + 20;
+    if ((val <= a - 100) && (conditionlow == 1)) {
       work_valve(5);
     }
-    if (val >= b) {                    //if the current value is less than the preset, turn the valve down
+    else {
+      conditionlow = 2;
+    }
+    if (((a - 100 < val) && (val < a)) && (conditionlow == 2)) {   //if the current value is less than the preset, turn on the up valve
+      pre_work_valve(5);
+    }
+    if ((val >= b + 100) && (conditionup == 1)) {                  //if the current value is less than the preset, turn the valve down
       work_valve(6);
+    }
+    else {
+      conditionup = 2;
+    }
+    if (((b < val) && (val < b + 100)) && (conditionup == 2)) {
+      pre_work_valve(6);
     }
     if ( b > val && val > a) {        //Suspension status indication
       digitalWrite(13, HIGH);
